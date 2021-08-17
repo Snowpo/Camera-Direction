bb036b8157d32fef40ee63b4d3c79dd1be96d651
Snowpo <77101984+Snowpo@users.noreply.github.com>
2021-08-17 15:49:55 +0530

    Add files via upload

---
+++ Bullet.js
@@ -0,0 +1,25 @@
+AFRAME.registerComponent('bullets',{
+    shootBullet: function(){
+        window.addEventListener('keydown',e=>{
+            if(e.key==='z'){
+                var bullet=document.createElement('a-entity')
+                bullet.setAttribute('geometry',{
+                    primitive:'sphere', radius:0.1
+                })
+                bullet.setAttribute('material','color','black')
+                var cam=document.querySelector('#camera')
+                pos=cam.getAttribute('position')
+                bullet.setAttribute('position', pos)
+                var camera=document.querySelector('#camera').object3D
+                var direction=new THREE.Vector3()
+                camera.getWorldDirection(direction)
+                bullet.setAttribute('velocity', direction.multiplyScalar(-10))
+                var scene = document.querySelector('#scene')
+                scene .appendChild(bullet)
+            }
+        })
+    },
+    init: function(){
+        this.shootBullet()
+    }
+})
